04b1cc2560e6cdbe5b8ee9914cd2373d470895ae
Matthew <mattgia3@gmail.com>
2020-10-27 22:14:07 -0400

    fix: listener

--- client/main.py
+++ client/main.py
@@ -1,22 +1,13 @@
-from scapy.all import ARP, Ether, sniff
-import time
+from scapy.all import sniff
+
+known_devices = set()
 
 def handle_arp_packet(packet):
-    is_arp = packet
-    print(is_arp)
-    print(dir(is_arp))
-    op = is_arp.op if is_arp else None
-    if op == ARP.who_has:
-        print('New ARP Request')
-        print(packet.summary())
-        print(packet[Ether].src, "has IP", packet[ARP].psrc)
-    elif op == ARP.is_at:
-        print('New ARP Reply')
-        print(packet.summary())
-    else:
-        print('No op found')
+    src = packet.src
+    if src != 'ff:ff:ff:ff:ff:ff':
+        known_devices.add(packet.src)
 
-    return
+    print(known_devices)
 
 if __name__ == "__main__":
     sniff(filter="arp", prn=handle_arp_packet)
